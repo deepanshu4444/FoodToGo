a1ccde62a44d0de2c065c74b2c544a2a659c1562
Manish Periwal <manishperiwal.mp@gmail.com>
2020-06-01 01:20:25 +0530

    toast

--- app/src/main/java/com/example/foodtogo/register.kt
+++ app/src/main/java/com/example/foodtogo/register.kt
@@ -1,12 +1,44 @@
 package com.example.foodtogo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.foodtogo.register
+import kotlinx.android.synthetic.main.activity_register.*
 
 class register : AppCompatActivity() {
+lateinit var name: EditText
+    lateinit var email: EditText
+    lateinit var mobile: EditText
+    lateinit var delivery: EditText
+lateinit var register :Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+            name = findViewById(R.id.mobile)
+        email = findViewById(R.id.email)
+        mobile = findViewById(R.id.mobile)
+        delivery = findViewById(R.id.delivery)
+        register = findViewById(R.id.register)
+        val name  = name.text.toString()
+        val email  = email.text.toString()
+        val mobile  = mobile.text.toString()
+        val delivery  = delivery.text.toString()
+
+        register.setOnClickListener {
+    val intent = Intent(this@register,register_detail::class.java)
+    startActivity(intent)
+            intent.putExtra("name","name")
+            intent.putExtra("email","email")
+            intent.putExtra("mobile","mobile")
+            intent.putExtra("delivery","delivery")
+
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/foodtogo/register_detail.kt
@@ -0,0 +1,28 @@
+package com.example.foodtogo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
+
+class register_detail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_detail)
+        if(intent!=null)
+        {
+            var name = intent.getStringExtra("name")
+            var email= intent.getStringExtra("email")
+            var mobile = intent.getStringExtra("mobile")
+            var delivery = intent.getStringExtra("delivery")
+            Toast.makeText(this@register_detail,name,Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@register_detail,email,Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@register_detail,mobile,Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@register_detail,delivery,Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+}
